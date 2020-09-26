92965ad5778ce4a91dc1e324e5d927ba843fc4ec
Omar Elfanek <elfanek@users.noreply.github.com>
2020-09-26 10:04:08 -0700

    Update scale animation to transform both x and y values (#76)
    Co-authored-by: JoniVR <joni.vr@hotmail.com>

--- Sources/VerticalCardSwiperFlowLayout.swift
+++ Sources/VerticalCardSwiperFlowLayout.swift
@@ -186,7 +186,8 @@ internal class VerticalCardSwiperFlowLayout: UICollectionViewFlowLayout {
             let translationScale = CGFloat((attributes.zIndex + 1) * 10)
             var t = CGAffineTransform.identity
 
-            t = t.scaledBy(x: scale > 0 ? scale : 0, y: 1)
+            let calculatedScale = scale > 0 ? scale : 0
+            t = t.scaledBy(x: calculatedScale, y: calculatedScale)
             if isStackingEnabled {
                 t = t.translatedBy(x: 0, y: top * translationScale)
             }
